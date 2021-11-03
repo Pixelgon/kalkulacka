8ea9918abf148a0d4675c44d750034195a9e6b4f
Pixelgon <pixelgon4@gmail.com>
2021-11-03 14:14:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# todo Přidat 3 3D nebo 2D tělesa
+
+print("Kalkulačka")
+cisloa = int(input("Zadejte první číslo: "))
+cislob = int(input("Zadejte druhé číslo: "))
+print("1. Součet")
+print("2. Součet")
+print("3. Součet")
+print("4. Součet")
+print("5. Součet")
+print("6. Součet")
+print("7. Součet")
+operace = int(input("Vyberte operaci: "))
+
+if operace == 1:
+print("Součet: ", cisloa, "+", cislob, "=", cisloa + cislob)
+elif operace ==
+print("Rozdíl: ", cisloa, "-", cislob, "=", cisloa - cislob)
+print("Součin: ", cisloa, "*", cislob, "=", cisloa * cislob)
+print("Dělení: ", cisloa, "/", cislob, "=", cisloa / cislob)
+print("Dělení se zbytkem: ", cisloa, "%", cislob, "=", cisloa // cislob, "(", cisloa % cislob, ")")
+print("Mocnina: ", "=", cisloa ** cislob)
+print("Odmocnina: ", "=", cislob ** (1/cisloa))
+
+
