5de6876e10b9a520e7953caf162377be8b30ea83
Pixelgon <pixelgon4@gmail.com>
2021-11-16 20:23:26 +0100

    Finished

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# todo Přidat 3 3D nebo 2D tělesa UI
+# Kalkulacka na obycejne operace a na vzorce
+# Vytvoril Matej Matejka
 import math
 
-print("| Kalkulačka |")
+print("|Kalkulačka|")
 print("1. obyčejný příklad")
 print("2. Různá tělesa")
-volba = int(input("Co potřebujete spočítat?:"))
+volba = int(input("Co potřebujete spočítat?: "))
 match volba:
     case 1:
         cisloa = int(input("Zadejte první číslo: "))
@@ -19,18 +20,25 @@ match volba:
         operace = int(input("Vyberte operaci: "))
 
         match operace:
+            # Soucete cisla A a B
             case 1:
                 print("Součet: ", cisloa, "+", cislob, "=", cisloa + cislob)
+            # Rozdil cisla A a B
             case 2:
                 print("Rozdíl: ", cisloa, "-", cislob, "=", cisloa - cislob)
+            # Soucin cisla A a B
             case 3:
                 print("Součin: ", cisloa, "*", cislob, "=", cisloa * cislob)
+            # Deleni cisla A
             case 4:
                 print("Dělení: ", cisloa, "/", cislob, "=", cisloa / cislob)
+            # Deleni cisla A se zbytkem
             case 5:
                 print("Dělení se zbytkem: ", cisloa, "%", cislob, "=", cisloa // cislob, "(", cisloa % cislob, ")")
+            # Mocnina
             case 6:
                 print("Mocnina: ", "=", cisloa ** cislob)
+            # Odmocnina
             case 7:
                 print("Odmocnina: ", "=", cislob ** (1/cisloa))
     case 2:
@@ -38,19 +46,49 @@ match volba:
         print("2. Obdelík")
         print("3. Kruh")
         tvar = int(input("Vyberte tvar: "))
-        stranaA = float(input("Zadejte stranu A"))
         match tvar:
+            # Ctverec
             case 1:
+                stranaA = float(input("Zadejte stranu A v centimetrech: "))
                 print("1. Obvod")
                 print("2. Obsah")
                 print("3. Úhlopříčka")
-                volbaVzorce = int(input("Vyberte vzorec"))
-                match volbaVzorce:
+                volbaVzorceCtverec = int(input("Vyberte vzorec: "))
+                match volbaVzorceCtverec:
                     case 1:
-                        print("Obvod čtverce je: ", stranaA * 4)
+                        print("Obvod čtverce je: ", stranaA * 4, "cm")
                     case 2:
-                        print("Obsah čtverce je:", stranaA * stranaA)
+                        print("Obsah čtverce je: ", stranaA * stranaA, "cm²")
                     case 3:
-                        print("Úhlopříčka čtverce je:", stranaA * math.sqrt(2))
+                        print("Úhlopříčka čtverce je: ", stranaA * math.sqrt(2), "cm")
+            # Obdelnik
+            case 2:
+                stranaA = float(input("Zadejte stranu A v centimetrech: "))
+                stranaB = float(input("Zadejte stranu B v centimetrech: "))
+                print("1. Obvod")
+                print("2. Obsah")
+                print("3. Úhlopříčka")
+                volbaVzorceObdelnik = int(input("Vyberte vzorec: "))
+                match volbaVzorceObdelnik:
+                    case 1:
+                        print("Obvod obdelníku je: ", stranaA * 2 + stranaB * 2, "cm")
+                    case 2:
+                        print("Obsah obdelníku je: ", stranaA * stranaB, "cm²")
+                    case 3:
+                        print("Úhlopříčka obdelníku je: ", stranaA**2 + 2**stranaB, "cm")
+            # Kruh
+            case 3:
+                polomerR = float(input("Zadejte poloměr v centimetrech: "))
+                print("1. Obvod")
+                print("2. Obsah")
+                print("3. Průměr")
+                volbaVzorceKruh = int(input("Vyberte vzorec: "))
+                match volbaVzorceKruh:
+                    case 1:
+                        print("Obvod kruhu je: ", 2 * math.pi * polomerR, "cm")
+                    case 2:
+                        print("Obsah kruhu je: ", math.pi * polomerR ** 2, "cm²")
+                    case 3:
+                        print("Průměr kruhu je: ", polomerR * 2, "cm")
 exit()
 
